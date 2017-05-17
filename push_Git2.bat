053f889640fcf0d12405c0a5cbb6ed4dc4f57618
chiou3qorz <chiou@e-happy.com.tw>
2017-05-17 10:35:21 +0800

    echo 10:35:21.10

---
+++ push_Git2.bat
@@ -0,0 +1,9 @@
+git status 
+git add .
+git commit -m  "echo %time%"
+git remote add origin https://github.com/chiou3qorz/secondgit.git
+
+git pull origin master
+
+git push -m origin master
+PAUSE
--- push_third.bat
+++
@@ -1,7 +0,0 @@
-git reset
-git status 
-git add .
-git commit -m  "echo %time%"
-git remote add origin https://github.com/chiou3qorz/third.git
-git push origin master
-PAUSE
